a16c0603c8d8a17faf0e841ae2cd53f8d5809aab
Amin-Ahmidout <AhmidoutA@hotmail.com>
2024-05-26 11:17:15 +0200

    cookid is nu automatisch het id van de user

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -62,23 +62,25 @@ let mealController = {
     },
 
     createMeal: (req, res, next) => {
-        const mealData = req.body
+        const mealData = req.body;
+        mealData.cookId = req.userId; // Stel de cookId in als de ingelogde gebruiker
+    
         mealService.createMeal(mealData, (error, success) => {
             if (error) {
                 return next({
                     status: error.status,
                     message: error.message,
                     data: {}
-                })
+                });
             }
             if (success) {
                 res.status(200).json({
                     status: 200,
                     message: success.message,
                     data: success.data
-                })
+                });
             }
-        })
+        });
     }
 }
 
