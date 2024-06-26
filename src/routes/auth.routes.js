ca9efbcf0a75a62964d02a8b3f3e20234982f040
Amin-Ahmidout <AhmidoutA@hotmail.com>
2024-06-26 22:23:23 +0200

    chore: Update Azure CLI login credentials and environment in workflow file

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -47,7 +47,7 @@ function validateToken(req, res, next) {
               logger.warn("Token invalid!" + err.message);
               next({
                   status: 401,
-                  message: "Token invalid!", //error message
+                  message: "Token invalid!", //
                   data: {},
               });
           }
